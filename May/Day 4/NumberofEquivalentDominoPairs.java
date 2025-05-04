1546728ffafc3fa52c5b8a12de4fa31f672987da
Shanmathik06 <shanmathi06.k@gmail.com>
2025-05-04 23:43:54 +0530

    Create NumberofEquivalentDominoPairs.java

---
+++ May/Day 4/NumberofEquivalentDominoPairs.java
@@ -0,0 +1,19 @@
+//Question:https://leetcode.com/problems/number-of-equivalent-domino-pairs/
+class Solution {
+    public long cantorPair(int a, int b) {
+        int x = Math.min(a, b), y = Math.max(a, b);
+        long sum = x + y;
+        return (sum * (sum + 1)) / 2 + y;
+    }
+
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int count = 0;
+        Map<Long, Integer> freq = new HashMap<>();
+        for (int[] dom : dominoes) {
+            long pairValue = cantorPair(dom[0], dom[1]);
+            count += freq.getOrDefault(pairValue, 0);
+            freq.put(pairValue, freq.getOrDefault(pairValue, 0) + 1);
+        }
+        return count;
+    }
+}
