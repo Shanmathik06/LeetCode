b1d2e9f255b6d089a8494e5465215e375c4b9927
Shanmathik06 <shanmathi06.k@gmail.com>
2025-06-01 22:46:22 +0530

    Create 2929.DistributeCandiesAmongChildrenII.java

---
+++ june/2929.DistributeCandiesAmongChildrenII.java
@@ -0,0 +1,11 @@
+Question : https://leetcode.com/problems/distribute-candies-among-children-ii/
+class Solution {
+    public long distributeCandies(int n, int limit) {
+        long res = 0;
+        for (int i = 0; i <= Math.min(limit, n); i++) {
+            if (n - i <= 2 * limit)
+                res += Math.min(n - i, limit) - Math.max(0, n - i - limit) + 1;
+        }
+        return res;
+    }
+}
