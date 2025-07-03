ac831b2c82923d047dd0eea6ee75f77177c8ed6b
Shanmathi K <shanmathi06.k@gmail.com>
2025-07-03 21:24:24 +0530

    Update 3304.FindtheK-thCharacterinStringGameI.java

--- July/Day_03/3304.FindtheK-thCharacterinStringGameI.java
+++ July/Day_03/3304.FindtheK-thCharacterinStringGameI.java
@@ -1,21 +1,21 @@
 //Question : https://leetcode.com/problems/find-the-k-th-character-in-string-game-i/
 class Solution {
     public char kthCharacter(int k) {
-        String word = "a";
-      return  helper(word,k);
-    }
-    public static char helper(String word,int k){
-        if(word.length()>=k){
-            return word.charAt(k-1);
+        StringBuilder str=new StringBuilder("a");
+        int n=str.length();
+        while(n<k){
+            StringBuilder append=new StringBuilder();
+            for(int i=0; i<n ;i++){
+                char ch = str.charAt(i);
+                int addNum=((ch-'a')+1)%26;
+               // System.out.print(addNum);
+                append.append((char)('a'+addNum));
+               //System.out.print((char)('a'+addNum));
+            }
+            str.append(append);
+            n=str.length();
+
         }
-        int index=0;
-        StringBuilder str = new StringBuilder(word);
-        while(index<word.length()){
-            char ch = word.charAt(index);
-            str.append((char)(ch + 1));    
-            index++;
-        }
-        return helper(str.toString(),k);
-        
+    return (str.toString().charAt(k-1));
     }
 }
