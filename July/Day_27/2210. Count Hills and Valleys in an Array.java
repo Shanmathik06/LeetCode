bda68fa1bd38c66bb25783318e9886eba9271921
Shanmathi K <shanmathi06.k@gmail.com>
2025-07-27 23:36:51 +0530

    Create 2210. Count Hills and Valleys in an Array.java

---
+++ July/Day_27/2210. Count Hills and Valleys in an Array.java
@@ -0,0 +1,18 @@
+//Question : https://leetcode.com/problems/count-hills-and-valleys-in-an-array/
+class Solution {
+    public int countHillValley(int[] nums) {
+        int count = 0;
+        int left = 0;
+
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                if ((nums[i] > nums[left] && nums[i] > nums[i + 1]) || 
+                    (nums[i] < nums[left] && nums[i] < nums[i + 1])) {
+                    count++;
+                }
+                left = i;
+            }
+        }
+        return count;
+    }
+}
